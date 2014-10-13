683287f16a31273d8697638dca13720a02ae53ec
stefan <stefan@illutek.be>
2014-10-13 23:02:53 +0200

    opzetten van node-dier.tpl.php

---
+++ node--dier.tpl.php
@@ -0,0 +1,8 @@
+<div  class="">
+      <div class="">
+          <h2 class="title_node"><?php //print $title; ?></h2>
+          <div class="dier_img"><?php print render($content['field_foto']);?></div>
+          <?php print render($content['body']);?>
+          <?php print render($content['field_aard']); ?>
+      </div>
+</div>
